f6c8e1640200abf37f4fa308218e3471b4c7300d
dansuar <danielsuarez@hotmail.com.ar>
2015-02-13 15:30:03 -0300

    mis primeros archivos

---
+++ inc/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content=" Daniel Suarez"/>
+	<meta name="viewport" content="device-width, minimum-scale=1, maximum-scale=1">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/estilos.css"/>
+
+    <title>CV Daniel A. Suarez.- Home</title>
+    
+</head>
+<body>
+<div class="contenedor">
+
+	<header class="cabecera">
+		
+		<figure class="figura2">
+			<img class="figura2__avatar" src="img/1/fondo-header.jpg" alt="Avatar">
+		</figure>
+		<!--<h1 class="h1_oficio">Analista de sistemas</h1>
+		<h2 class="h2_nombre">Daniel Suarez</h2>-->
+	
+	
+	
+	</header>
+	<figure class="figura1">
+		<img class="figura1__logo" src="img/logo-ds-14.png" alt="Logo">
+	</figure>
+	<nav class="navegacion">
+		<ul class="menu">
+			<li class="menu__home"><a href="#">Home</a></li>
+			<li class="menu__porfolio"><a href="#">Porfolio</a></li>
+			<li class="menu__estudio"><a href="#">Estudios</a></li>
+			<li class="menu__d-personales"><a href="#">Datos</a></li>
+			<li class="menu__contactar"><a href="#">Contactar</a></li>
+		</ul>
+	</nav>
+</div>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include("inc/header.php");?>
+<section>
+	<article class="art_home">art_home</article>
+	<article class="art_porfolio">art_porfolio</article>
+	<article class="art_estudios">art_estudios</article>
+	<article class="art_datos">art_datos</article>
+	<article class="art_contactar">art_contactar</article>
+</section>
+<?php include("inc/footer.php");?>
